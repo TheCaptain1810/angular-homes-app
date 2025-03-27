178b06f93fb04e09d41ca25de6cdfd62f18cfc7a
Pranav Pise <itspranavpise@gmail.com>
2025-03-27 14:57:33 +0530

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
+import { RouterModule } from "@angular/router";
 
 @Component({
   standalone: true,
@@ -15,11 +16,11 @@ import { HomeComponent } from "./home/home.component";
         />
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
